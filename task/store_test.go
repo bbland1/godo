b9f22edbb9d298de8bb5a01bfd1006b998fe8e3f
bbland1 <104288486+bbland1@users.noreply.github.com>
2024-11-28 01:39:09 -0500

    adding tests for adding a valid/invalid task

--- task/store_test.go
+++ task/store_test.go
@@ -1,9 +1,11 @@
 package task
 
 import (
-	"database/sql"
+	"strings"
 	"testing"
-	
+	"time"
+
+	"github.com/google/uuid"
 	_ "modernc.org/sqlite" // Import SQLite driver
 )
 
@@ -29,33 +31,162 @@ func TestDatabaseInit(t *testing.T) {
 }
 
 func TestGetAllTasks(t *testing.T) {
+	db, err := InitDatabase(":memory:")
+	if err != nil {
+		t.Fatalf("InitDatabase failed at creating the db, %v", err)
+	}
 
+	defer db.Close()
 }
 
 func TestGetATask(t *testing.T) {
+	db, err := InitDatabase(":memory:")
+	if err != nil {
+		t.Fatalf("InitDatabase failed at creating the db, %v", err)
+	}
 
+	defer db.Close()
 }
 
 func TestUpdateTaskName(t *testing.T) {
+	db, err := InitDatabase(":memory:")
+	if err != nil {
+		t.Fatalf("InitDatabase failed at creating the db, %v", err)
+	}
 
+	defer db.Close()
 }
 
 func TestUpdateTaskComplete(t *testing.T) {
+	db, err := InitDatabase(":memory:")
+	if err != nil {
+		t.Fatalf("InitDatabase failed at creating the db, %v", err)
+	}
 
+	defer db.Close()
 }
 
 func TestDeleteTask(t *testing.T) {
+	db, err := InitDatabase(":memory:")
+	if err != nil {
+		t.Fatalf("InitDatabase failed at creating the db, %v", err)
+	}
 
+	defer db.Close()
 }
 
 func TestAddTask(t *testing.T) {
+	db, err := InitDatabase(":memory:")
+	if err != nil {
+		t.Fatalf("InitDatabase failed at creating the db, %v", err)
+	}
 
+	defer db.Close()
+
+	testName := "tester name"
+
+	testTask := CreateTask(testName)
+
+	err = AddTask(db, testTask)
+	if err != nil {
+		t.Fatalf("AddTask failed: %v", err)
+	}
+
+	query := `SELECT id, name, is_completed FROM tasks WHERE id = ?`
+	row := db.QueryRow(query)
+
+	var id, name string
+	var isCompleted bool
+	err = row.Scan(&id, &name, &isCompleted)
+
+	if id != testTask.ID || name != testTask.Name || isCompleted != testTask.IsCompleted {
+		t.Errorf("Expected task (%v), got (%v, %v, %v)", testTask, id, name, isCompleted)
+	}
 }
 
 func TestAddingDuplicateTask(t *testing.T) {
+	db, err := InitDatabase(":memory:")
+	if err != nil {
+		t.Fatalf("InitDatabase failed at creating the db, %v", err)
+	}
 
+	defer db.Close()
+
+	testName := "tester name"
+
+	testTask := CreateTask(testName)
+
+	err = AddTask(db, testTask)
+	if err != nil {
+		t.Fatalf("AddTask failed: %v", err)
+	}
+
+	duplicateTestTask := &Task{
+		ID:            testTask.ID,
+		Name:          "duplication",
+		IsCompleted:   false,
+		DateAdded:     time.Now(),
+		DateCompleted: nil,
+	}
+
+	err = AddTask(db, duplicateTestTask)
+	if err == nil {
+		t.Errorf("Expected there to be an error when adding a duplicate task but got none")
+	}
+
+	if err != nil && !strings.Contains(err.Error(), "UNIQUE constraint failed") {
+		t.Errorf("Expected unique constraint error, got: %v", err)
+	}
 }
 
-func TestAddInvalidTask(t *testing.T) {
-	
+func TestAddInvalidIDTask(t *testing.T) {
+	db, err := InitDatabase(":memory:")
+	if err != nil {
+		t.Fatalf("InitDatabase failed at creating the db, %v", err)
+	}
+
+	defer db.Close()
+
+	testTask := &Task{
+		ID:            "7",
+		Name:          "duplication",
+		IsCompleted:   false,
+		DateAdded:     time.Now(),
+		DateCompleted: nil,
+	}
+
+	err = AddTask(db, testTask)
+	if err == nil {
+		t.Errorf("Expected there to be an error when adding a task with an invalid ID (non-UUID), but got none")
+	}
+
+	if err != nil && strings.Contains(err.Error(), "invalid UUID") {
+		t.Errorf("Expected unique constraint error, got: %v", err)
+	}
+}
+
+func TestAddEmptyNameTask(t *testing.T) {
+	db, err := InitDatabase(":memory:")
+	if err != nil {
+		t.Fatalf("InitDatabase failed at creating the db, %v", err)
+	}
+
+	defer db.Close()
+
+	testTask := &Task{
+		ID:            uuid.New().String(),
+		Name:          "",
+		IsCompleted:   false,
+		DateAdded:     time.Now(),
+		DateCompleted: nil,
+	}
+
+	err = AddTask(db, testTask)
+	if err == nil {
+		t.Errorf("Expected there to be an error when adding a task with an empty Name, but got none")
+	}
+
+	if err != nil && strings.Contains(err.Error(), "CHECK constraint failed") {
+		t.Errorf("Expected CHECK constraint error for Name, got: %v", err)
+	}
 }
