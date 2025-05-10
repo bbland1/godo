ab45de36285c563bff42526fdf575fd123eaa140
bbland1 <104288486+bbland1@users.noreply.github.com>
2025-05-10 19:15:47 -0400

    feat: making sure see all the tasks when no args passed

--- cmd/view.go
+++ cmd/view.go
@@ -16,9 +16,59 @@ usage:
 	
 options:
 	-h 	show the usage for the command
-	-id show all the information for the specific task`
+	-id show all the information for the specific task
+	-status show all the tasks with a specific status`
 
-func viewFunc(database *sql.DB, args []string, cmd *BaseCommand) int {
+func viewFunc(stderr io.Writer, database *sql.DB, args []string, cmd *BaseCommand) int {
+
+	idFlagValue := cmd.flags.Lookup("id").Value.String()
+	statusFlagValue := cmd.flags.Lookup("status").Value.String()
+
+	// if idFlagValue == "" && statusFlagValue == "" {
+	// 	tasks, err := task.GetAllTasks(database)
+	// 	if err != nil {
+	// 		fmt.Fprintf(cmd.errOutput, "database error: %v\n", err)
+	// 		return 1
+	// 	}
+	// 	tw := tabwriter.NewWriter(cmd.output, 0, 8, 2, ' ', 0)
+	// 	fmt.Fprintln(tw, "ID\tTask Description\tStatus")
+
+	// 	for _, storedTask := range tasks {
+	// 		fmt.Fprintf(tw, "%d\t%s\t%t\n", storedTask.ID, storedTask.Description, storedTask.IsCompleted)
+	// 	}
+
+	// 	tw.Flush()
+	// 	return 0
+	// }
+
+	if idFlagValue != "" && statusFlagValue != "" {
+		fmt.Fprintln(stderr, "passing an id & status argument causes conflict, try again passing one or the other")
+		return 1
+	}
+
+	if idFlagValue != "" {
+		// storedTask, err := task.GetATaskByID(database, in)
+	}
+
+	// if len(args) == 0 || args[0] == "" {
+	// 	tasks, err := task.GetAllTasks(database)
+	// 	if err != nil {
+	// 		fmt.Fprintf(cmd.errOutput, "database error: %v\n", err)
+	// 		return 1
+	// 	}
+	// 	tw := tabwriter.NewWriter(cmd.output, 0, 8, 2, ' ', 0)
+	// 	fmt.Fprintln(tw, "ID\tTask Description\tStatus")
+
+	// 	for _, storedTask := range tasks {
+	// 		fmt.Fprintf(tw, "%d\t%s\t%t\n", storedTask.ID, storedTask.Description, storedTask.IsCompleted)
+	// 	}
+
+	// 	tw.Flush()
+	// 	return 0
+	// } 
+
+	
+
 	tasks, err := task.GetAllTasks(database)
 	if err != nil {
 		fmt.Fprintf(cmd.errOutput, "database error: %v\n", err)
@@ -37,15 +87,18 @@ func viewFunc(database *sql.DB, args []string, cmd *BaseCommand) int {
 
 func NewViewCommand(stdout, stderr io.Writer, db *sql.DB, exitCode *int) *BaseCommand {
 	command := &BaseCommand{
-		name: "view",
+		name:        "view",
 		description: "view all tasks or a specific task",
-		flags: flag.NewFlagSet("view", flag.ExitOnError),
-		output: stdout,
-		errOutput: stderr,
+		flags:       flag.NewFlagSet("view", flag.ExitOnError),
+		output:      stdout,
+		errOutput:   stderr,
 		execute: func(cmd *BaseCommand, args []string) {
-			*exitCode = viewFunc(db, args, cmd)
+			*exitCode = viewFunc(cmd.errOutput, db, args, cmd)
 		},
 	}
+
+	command.flags.Int64("id", 0, "id fo the specific task to look at")
+	command.flags.String("status", "", "completion status to filter the list on")
 
 	command.flags.Usage = func() {
 		fmt.Fprintln(command.output, ViewUsage)
--- cmd/view_test.go
+++ cmd/view_test.go
@@ -72,7 +72,6 @@ func TestViewCommandNoArgs(t *testing.T) {
 	addCommand := NewAddCommand(&bufferOut, &bufferErr, db, &exitCode)
 
 	addCommand.execute(addCommand, []string{"tester"})
-	addCommand.execute(addCommand, []string{"tester 1"})
 
 	viewCommand := NewViewCommand(&bufferOut, &bufferErr, db, &exitCode)
 
@@ -85,7 +84,7 @@ func TestViewCommandNoArgs(t *testing.T) {
 		t.Errorf("There has been an issue getting the tasks from the DB, %s", err)
 	}
 	tw := tabwriter.NewWriter(&bufExpOut, 0, 8, 2, ' ', 0)
-	fmt.Fprintln(tw, "ID\tTask Description\tStatusgi")
+	fmt.Fprintln(tw, "ID\tTask Description\tStatus")
 
 	for _, storedTask := range tasks {
 		fmt.Fprintf(tw, "%d\t%s\t%t\n", storedTask.ID, storedTask.Description, storedTask.IsCompleted)
@@ -93,6 +92,48 @@ func TestViewCommandNoArgs(t *testing.T) {
 
 	tw.Flush()
 
+	if exitCode != 0 {
+		t.Errorf("Expected exit code to be: 0, got: %d", &exitCode)
+	}
+
+	output := strings.TrimSpace(bufferOut.String())
+	expectedOutput := strings.TrimSpace(bufExpOut.String())
+
+	if output != expectedOutput {
+		t.Errorf("Expected output: %q, got: %q", expectedOutput, output)
+	}
+}
+
+func TestViewCommandBothArgs(t *testing.T) {
+	var bufferOut bytes.Buffer
+	var bufferErr bytes.Buffer
+	var exitCode int
+
+	db, err := task.InitDatabase(":memory:")
+	if err != nil {
+		t.Fatalf("InitDatabase failed at creating the db, %v", err)
+	}
+
+	defer db.Close()
+
+	addCommand := NewAddCommand(&bufferOut, &bufferErr, db, &exitCode)
+
+	addCommand.execute(addCommand, []string{"tester"})
+	addCommand.execute(addCommand, []string{"tester 1"})
+
+	viewCommand := NewViewCommand(&bufferOut, &bufferErr, db, &exitCode)
+
+	viewCommand.Init([]string{"-id=1"})
+	viewCommand.Run()
+
+	var bufExpOut bytes.Buffer
+
+	tw := tabwriter.NewWriter(&bufExpOut, 0, 8, 2, ' ', 0)
+	fmt.Fprintln(tw, "ID\tTask Description\tStatus")
+
+	fmt.Fprintf(tw, "%d\t%s\t%t\n", 1, "tester", false)
+
+	tw.Flush()
 
 	if exitCode != 0 {
 		t.Errorf("Expected exit code to be: 0, got: %d", &exitCode)
